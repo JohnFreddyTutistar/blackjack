145bfade923a5233e8eb5843a4e6e34d66864abf
JohnFreddyTutistar <johnfre.157@gmail.com>
2025-08-23 00:01:01 -0500

    alandiendo nuevas funciones

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -1,9 +1,18 @@
-let deck = [];
 
+/**
+ * C: clover = trebol
+ * D: diamonts = diamantes
+ * H: hearts = Corazones
+ * E: spades = Espadas
+*/
 const tipos = [ 'C', 'D', 'H', 'E' ]
 
+// Cartas especiales
 const especiales = ['A', 'J', 'Q', 'K']
 
+let deck = [];
+
+// Crea nuevo deck
 const crearDeck = () => {
   
     for(let i = 2; i <= 10; i++){
@@ -18,11 +27,41 @@ const crearDeck = () => {
         }
     } 
 
-    console.log(deck);
+    // console.log(deck);
 
     deck = _.shuffle( deck )
     console.log(deck);
 
+    return deck
+
 }
 
 crearDeck()
+
+// Esta función me permite tomar una carta
+const pedirCarta = () => {
+
+    if(deck.length === 0) throw new Error('No hay cartas en el deck') 
+    
+    const carta = deck.pop();
+
+
+    console.log(deck)
+
+    console.log(carta)
+
+  return carta
+}
+
+pedirCarta()
+
+const valorCarta = ( carta ) => {
+    
+    const valor = carta.substring( 0, carta.length - 1 );
+    return ( isNaN(valor)) ? 
+            (valor === 'A') ? 11 : 10
+            : valor * 1;
+}
+
+const valor = valorCarta( pedirCarta() );
+console.log({ valor }) 

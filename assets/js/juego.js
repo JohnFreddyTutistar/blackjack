9b9481fe942d2d03600edcdc5133d3deb65c3a39
JohnFreddyTutistar <johnfre.157@gmail.com>
2025-08-25 16:37:20 -0500

    refactorizando el codigo

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -5,164 +5,171 @@
  * H: hearts = Corazones
  * E: spades = Espadas
 */
-const tipos      = [ 'C', 'D', 'H', 'S' ]
-const especiales = ['A', 'J', 'Q', 'K']
 
-let puntosJugador      = 0;
-    puntosComputadora  = 0;
+// PATRON MÓDULO: Función anonima autoinvocada: crea un nuevo scope el cual no tiene una referencia por nombre
+(() => {
 
-// Referencias del HTML
-const mostrarPuntos         = document.querySelectorAll('small');
-const divCartasJugador      = document.querySelector('#jugador-cartas')
-const divCartasComputadora  = document.querySelector('#computador-cartas')
-const btnDetener            = document.querySelector('#btnDetener');
-const btnNuevo              = document.querySelector('#btnNuevo');
+    let deck         = [];
+    const tipos      = [ 'C', 'D', 'H', 'S' ],
+          especiales = ['A', 'J', 'Q', 'K']
+    
+    let puntosJugador      = 0,
+        puntosComputadora  = 0;
 
-let deck = [];
+    // let puntosJugadores = [];
+    
+    // Referencias del HTML
+    const mostrarPuntos         = document.querySelectorAll('small'),
+          divCartasJugadores    = document.querySelector('#jugador-cartas'),
+          divCartasComputadora  = document.querySelector('#computador-cartas'),
+          btnNuevo              = document.querySelector('#btnNuevo'),
+          btnPedir              = document.querySelector('#btnPedir'),
+          btnDetener            = document.querySelector('#btnDetener');
 
-// Crea nuevo deck
-const crearDeck = () => {
-  
-    for(let i = 2; i <= 10; i++){
-        for(let tipo of tipos){
-            deck.push(i + tipo)
-        }
+    // Función para inicializar el juego
+    const inicializarJuego = ( numJugadores = 2 ) => {
+      deck = crearDeck()
     }
 
-    for( tipo of tipos ){
-        for( especial of especiales){
-            deck.push(especial + tipo)
+    // const puntosComputadora = puntosJugadores.length - 1
+    
+    
+    // Crea nuevo deck
+    const crearDeck = () => {
+      
+        deck = [];
+        for(let i = 2; i <= 10; i++){
+            for(let tipo of tipos){
+                deck.push(i + tipo)
+            }
         }
-    } 
-
-    // console.log(deck);
-
-    deck = _.shuffle( deck )
-
-    return deck
-
-}
-
-crearDeck()
-
-// Esta función me permite tomar una carta
-const pedirCarta = () => {
-
-    if(deck.length === 0) throw new Error('No hay cartas en el deck') 
     
-    const carta = deck.pop();
-
-  return carta
-}
-
-pedirCarta()
-
-const valorCarta = ( carta ) => {
+        for( tipo of tipos ){
+            for( especial of especiales){
+                deck.push(especial + tipo)
+            }
+        } 
     
-    const valor = carta.substring( 0, carta.length - 1 );
-    return ( isNaN(valor)) ? 
-            (valor === 'A') ? 11 : 10
-            : valor * 1;
-}
+        return _.shuffle( deck )
+    }
 
-const valor = valorCarta( pedirCarta() );
+    // crearDeck()
+    
+    
+    // Esta función me permite tomar una carta
+    const pedirCarta = () => {
+    
+        if(deck.length === 0) throw new Error('No hay cartas en el deck') 
+        // Vamos a tomar la ultima carta del maso 
+        return deck.pop()
+    }
+    
+    const valorCarta = ( carta ) => {
+        
+        const valor = carta.substring( 0, carta.length - 1 );
+        return ( isNaN(valor)) ? 
+                (valor === 'A') ? 11 : 10
+                : valor * 1;
+    }
+    
+    // Turno de la computadora
+    const turnoComputadora = ( puntosMinimos ) => {
+    
+         do {
+            const carta = pedirCarta()
+    
+            puntosComputadora = puntosComputadora + valorCarta( carta )
+            
+            mostrarPuntos[1].innerHTML = `<b>${puntosComputadora}</b>`
 
-// Turno de la computadora
-const turnoComputadora = ( puntosMinimos ) => {
+            const imgCarta = document.createElement('img');
+            imgCarta.src = `assets/cartas/${carta}.png`
+            imgCarta.classList.add('carta')
+            divCartasComputadora.append(imgCarta)
+            
+            if( puntosMinimos > 21 ){
+                break;
+            }
+    
+         } while( (puntosComputadora < puntosMinimos) && (puntosMinimos <= 21) )
+    
+    }
+    
+    
+    // Eventos
+    btnPedir.addEventListener('click', () => {
 
-     do {
-        const carta = pedirCarta()
+        const carta = pedirCarta();
+        
+        puntosJugador = puntosJugador + valorCarta( carta )
 
-        puntosComputadora = puntosComputadora + valorCarta( carta );
-
-        mostrarPuntos[1].innerHTML = `<b>${puntosComputadora}</b>`
+        mostrarPuntos[0].innerHTML = `<b>${puntosJugador}</b>`
 
         const imgCarta = document.createElement('img');
-        imgCarta.src = `assets/cartas/${carta}.png`
-        imgCarta.classList.add('carta')
-        divCartasComputadora.append(imgCarta)
-
-        if( puntosMinimos > 21 ){
-            break;
-        }
-
-     } while( (puntosComputadora < puntosMinimos) && (puntosMinimos <= 21) )
-
-}
-
-
-// Eventos
-btnPedir.addEventListener('click', () => {
-    const carta = pedirCarta();
-    console.log('carta seleccionada: ', carta)
-
-    puntosJugador = puntosJugador + valorCarta( carta );
-
-    mostrarPuntos[0].innerHTML = `<b>${puntosJugador}</b>`
-
-    const imgCarta = document.createElement('img');
-    imgCarta.src = `assets/cartas/${carta}.png`
-    imgCarta.classList.add('carta')
-    divCartasJugador.append(imgCarta)
-
-    if( puntosJugador>21 ){
-        Swal.fire({
-            icon: "error",
-            title: "Oops...",
-            text: "Perdiste, intentalo nuevamente",
-        });
-        btnPedir.disabled   = true
-        btnDetener.disabled = true;
-        turnoComputadora( puntosJugador )
-    } else if (puntosJugador === 21) {
-        Swal.fire({
-            title: "Drag me!",
-            icon: "success",
-            draggable: true
-        });
-        console.warn('genial')
-        btnPedir.disabled   = true
-        btnDetener.disabled = true;
-        turnoComputadora( puntosJugador )
-    }
-
-
-})
-
-// función para detener el juego
-btnDetener.addEventListener('click', () => {
-
-    if(puntosJugador <= 21){ 
-        btnDetener.disabled = true;
-        btnPedir.disabled = true;
-        turnoComputadora( puntosJugador )
-        if( puntosComputadora === puntosJugador ) {
+        imgCarta.src = `assets/cartas/${carta}.png`;
+        imgCarta.classList.add('carta');
+        divCartasJugadores.append( imgCarta )
+    
+        if( puntosJugador>21 ){
             Swal.fire({
-                title: "Empate. nadie gana",
-                icon: "alert",
-                draggable: true
+                icon: "error",
+                title: "Oops...",
+                text: "Perdiste, intentalo nuevamente",
             });
-        } else if( (puntosComputadora > puntosJugador)  && (puntosComputadora <= 21) ){
+            btnPedir.disabled   = true
+            btnDetener.disabled = true;
+            turnoComputadora( puntosJugador )
+        } else if (puntosJugador === 21) {
             Swal.fire({
-                title: "Conputadora gana",
+                title: "Ganaste!",
                 icon: "success",
                 draggable: true
             });
-        } else {
-            Swal.fire({
-                title: "Felicitaciones, jugador gana",
-                icon: "success",
-                draggable: true
-            });
+            btnPedir.disabled   = true
+            btnDetener.disabled = true;
+            turnoComputadora( puntosJugador )
         }
-    }
+    
+    
+    })
+    
+    // función para detener el juego
+    btnDetener.addEventListener('click', () => {
+    
+        if(puntosJugador <= 21){ 
+            btnDetener.disabled = true;
+            btnPedir.disabled = true;
+            turnoComputadora( puntosJugador )
 
-})
+            if( puntosComputadora === puntosJugador ) {
+                Swal.fire({
+                    title: "Empate. nadie gana",
+                    icon: "alert",
+                    draggable: true
+                });
+            } else if( (puntosComputadora > puntosJugador)  && (puntosComputadora <= 21) ){
+                Swal.fire({
+                    title: "Conputadora gana",
+                    icon: "success",
+                    draggable: true
+                });
+            } else {
+                Swal.fire({
+                    title: "Felicitaciones, jugador gana",
+                    icon: "success",
+                    draggable: true
+                });
+            }
+        }
+    
+    })
 
-// Función nuevo juego
-btnNuevo.addEventListener('click', () => {
-    location.reload()
-})
+    inicializarJuego()
+    
+    // Función nuevo juego
+    btnNuevo.addEventListener('click', () => {
+        inicializarJuego()
+        location.reload()
+    })
 
-
-// TODO Borrar
+})();
